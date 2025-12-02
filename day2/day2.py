dc36f390b7f34f7d55727c9fb5401d11321ee396
abdullah-25 <mabdullah@tvo.org>
2025-12-02 17:56:08 -0500

    day 2 part2

--- day2/day2.py
+++ day2/day2.py
@@ -7,8 +7,23 @@
 
 
 def repeating_numbers_check(str_number):
+    """Check if number is made of some sequence repeated at least twice.
+    Examples: 12341234 (1234 twice), 123123123 (123 three times), 1212 (12 twice)
+    """
     n = len(str_number)
-    return str_number[:n//2] == str_number[n//2:]
+
+    # Pattern must repeat at least twice, so max length is n//2
+    for pattern_len in range(1, n // 2 + 1):
+        # Check if the string length is divisible by pattern length
+        print(pattern_len)
+        if n % pattern_len == 0:
+            pattern = str_number[:pattern_len]
+            # Check if repeating this pattern gives us the whole string
+            if pattern * (n // pattern_len) == str_number:
+                return True
+
+    return False
+
 
 
 def add_invalid_ids(input_ids):
@@ -17,18 +32,12 @@ def add_invalid_ids(input_ids):
     for id in input_ids:
         id1, id2 = id.split('-')
         for i in range(int(id1), int(id2)+1):
+            if str(i) == '101':
+                continue
             if repeating_numbers_check(str(i)):
                 print(f'invalid id: {i}')
                 sum += i
     return sum
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -39,7 +48,6 @@ if __name__ == "__main__":
             for item in data:
                 if item != '':
                     input_ids.append(item)
+    print(repeating_numbers_check('565656'))
+    # print(add_invalid_ids(input_ids))
 
-    print(add_invalid_ids(input_ids))
-
-        
