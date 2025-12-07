e722f59e3c5d88cef9c107ddf96250aa41066d6c
abdullah-25 <mabdullah@tvo.org>
2025-12-07 00:07:53 -0500

    day5 problem 2

--- day5/day5.py
+++ day5/day5.py
@@ -4,8 +4,7 @@ def count_fresh_ids(id_range, id_list):
 
     for start, end in id_range:
         normalized_ranges.append((int(start) , int(end)))
-    
-    
+
     for id in id_list:
         for start, end in normalized_ranges:
             if start <= id <= end:
@@ -15,11 +14,31 @@ def count_fresh_ids(id_range, id_list):
     return fresh_count
 
 
+def count_fresh_ids_part_2(id_range):
+    normalized_ranges = []
+    res = int()
 
+    for start, end in id_range:
+        normalized_ranges.append([int(start), int(end)])
 
+    normalized_ranges_sort = sorted(normalized_ranges)
+    intersect = [normalized_ranges_sort[0]]
+
+    for i in range(1, len(normalized_ranges_sort)):
+        last = intersect[-1]
+        curr = normalized_ranges_sort[i]
+        if curr[0] <= last[1]:
+            last[1] = max(last[1], curr[1])
+        else:
+            intersect.append(curr)
+
+    for item in intersect:
+        res += (item[1] - item[0] + 1)
+
+    return res
 
 if __name__ == '__main__':
-    with open ('test.txt', 'r', encoding='UTF8') as f:
+    with open('test.txt', 'r', encoding='UTF8') as f:
         id_range = []
         id_to_check = False
         id_list = []
@@ -33,4 +52,5 @@ if __name__ == '__main__':
             
             if not id_to_check:
                 id_range.append(line.strip().split('-'))
-    print(count_fresh_ids(id_range, id_list))
+    # print(count_fresh_ids(id_range, id_list))
+    print(count_fresh_ids_part_2(id_range))
